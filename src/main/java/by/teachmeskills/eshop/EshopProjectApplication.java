37973be3005b2c646dc50d17325ea9f6b7951d54
tsarikevich <ma.tsarikevich@gmail.com>
2022-08-08 15:05:43 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/eshop/EshopProjectApplication.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopProjectApplication.class, args);
+    }
+
+}
